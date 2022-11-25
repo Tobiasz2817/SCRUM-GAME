1912cb4c94b154857258e29d598e3de8efb98373
Calsyd <72507262+Calsyd@users.noreply.github.com>
2022-11-25 21:01:23 +0100

    complete menu UI
    Do zrobienia:
    - dostosowanie wielkości paneli
    - wrzucenie GAME, PAUSE I SHIFT TOWERS do sceny z grą

---
+++ Assets/Scripts/Main menu/button.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
